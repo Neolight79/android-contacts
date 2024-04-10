1a5d1acb9bda6376a82133ebfd59e0275e14ec14
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-04-10 23:29:29 +0300

    Class ListDiffCallback() within adapters: ContactAdapter, FilterContactTypeAdapter, SortTypeAdapter was replaced by generic class BaseListDiffCallback<> bounded by generic interface ListDiffInterface<>. Functions theSameAs() have been corrected for FilterContactTypeUI and FilterContactTypeUI: hashCode() was replaced by appropriate method. Parameter name changed for theSameAs().

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
-    boolean theSameAs(T t);
+    boolean theSameAs(T anotherItem);
 
     boolean equals(Object obj);
 
